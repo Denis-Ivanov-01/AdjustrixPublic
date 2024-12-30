b5c317869ab5e06a47cdee4882b5281fb8ff9c0b
Denis Ivanov <denis.ivanof2001@gmail.com>
2024-12-30 18:06:10 +0200

    project button change

--- AdjustrixWPF/ViewModel/MainWindowViewModel.cs
+++ AdjustrixWPF/ViewModel/MainWindowViewModel.cs
@@ -3,11 +3,13 @@
     public class MainWindowViewModel : ViewModelBase
     {
         public LanguageViewModel languageViewModel { get; set; }
+        public ProjectViewModel projectViewModel { get; set; }
         public SystemFileManagement SystemFileSingleton { get; set; }
 
         public MainWindowViewModel()
         {
             languageViewModel = new LanguageViewModel();
+            projectViewModel = new ProjectViewModel();
             SystemFileSingleton = SystemFileManagement.Singleton;
         }
     }
---
+++ AdjustrixWPF/ViewModel/ProjectViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdjustrixWPF.ViewModel
+{
+    public class ProjectViewModel : ViewModelBase
+    {
+        private bool projectLoaded=false;
+
+        public ProjectViewModel()
+        {
+
+        }
+
+
+        public bool ProjectLoaded
+        {
+            get { return projectLoaded; }
+            set { projectLoaded = value; }
+        }
+
+    }
+}
