8558a027235c9bf5c151973b09c3b2c61b2700ea
Denis Ivanov <denis.ivanof2001@gmail.com>
2025-01-23 15:59:20 +0200

    add labeled textbox row, refactor create/edit prompts

---
+++ AdjustrixWPF/View/UserControls/LabeledTextBoxRow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AdjustrixWPF.View.UserControls
+{
+    /// <summary>
+    /// Interaction logic for LabeledTextBoxRow.xaml
+    /// </summary>
+    public partial class LabeledTextBoxRow : UserControl
+    {
+
+
+
+        public string TextBoxText
+        {
+            get { return (string)GetValue(TextBoxTextProperty); }
+            set { SetValue(TextBoxTextProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for TextBoxText.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty TextBoxTextProperty =
+            DependencyProperty.Register("TextBoxText", typeof(string), typeof(LabeledTextBoxRow), new PropertyMetadata(""));
+
+
+
+        public string LabelText
+        {
+            get { return (string)GetValue(LabelTextProperty); }
+            set { SetValue(LabelTextProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for LabelText.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty LabelTextProperty =
+            DependencyProperty.Register("LabelText", typeof(string), typeof(LabeledTextBoxRow), new PropertyMetadata(""));
+
+        public LabeledTextBoxRow()
+        {
+            InitializeComponent();
+        }
+    }
+}
